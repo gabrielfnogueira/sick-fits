70966eddea76c3e5099d0f2f4d25b2f26aa9e89a
Gabriel Nogueira <gfnogueira@gmail.com>
2018-11-27 10:00:37 -0200

    feat: add ability to update items

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,8 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
-  items: forwardTo('db')
+  items: forwardTo('db'),
+  item: forwardTo('db')
   // async items(pareant, args, ctx, info) {
   //   const items = await ctx.db.query.items();
   //   return items;
