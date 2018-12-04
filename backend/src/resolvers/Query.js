539aa2b14e9e6dfe487800cc0b0fe4d7fb5adda7
Gabriel Nogueira <gfnogueira@gmail.com>
2018-12-04 08:36:22 -0200

    feat: add Pagination component

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,7 +2,8 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
   items: forwardTo('db'),
-  item: forwardTo('db')
+  item: forwardTo('db'),
+  itemsConnection: forwardTo('db')
   // async items(pareant, args, ctx, info) {
   //   const items = await ctx.db.query.items();
   //   return items;
