b1f3a9e98f34bb07c2124e1c4d4f9fc7659c77d6
Gabriel Nogueira <gfnogueira@gmail.com>
2019-01-03 10:29:38 -0200

    feat: add gated sign in component

---
+++ frontend/components/PleaseSignIn.js
@@ -0,0 +1,23 @@
+import { Query } from 'react-apollo';
+import Signin from './Signin';
+import { CURRENT_USER_QUERY } from './User';
+
+const PleaseSignIn = props => (
+  <Query query={CURRENT_USER_QUERY}>
+    {({ data, loading }) => {
+      if (loading) return <p>Loading...</p>;
+      if (!data.me) {
+        return (
+          <div>
+            <p>Please Sign In before Continuing</p>
+            <Signin />
+          </div>
+        );
+      }
+
+      return props.children;
+    }}
+  </Query>
+);
+
+export default PleaseSignIn;
